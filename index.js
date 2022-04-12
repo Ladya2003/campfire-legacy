3bba7842605ed37f905d4f0a36ba0b299951f1f5
Ladya2003 <92051143+Ladya2003@users.noreply.github.com>
2022-04-12 13:39:26 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     $("#nav__body").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1500);
     });
-});
+});*/
